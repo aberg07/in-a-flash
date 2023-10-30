4f22f8b7f5fd8efe839eb3af038c85719b5711fe
aberg07 <140882154+aberg07@users.noreply.github.com>
2023-10-30 20:31:15 +0900

    Adding decks and switching between decks now works as intended. Does not yet save multiple decks.

--- Scripts/script.js
+++ Scripts/script.js
@@ -9,6 +9,10 @@ const frontText = document.getElementById('front-text');
 const backText = document.getElementById('back-text');
 const submitButton = document.getElementById('submit-button');
 submitButton.addEventListener('click', () => addCard())
+const deck1Button = document.getElementById('deck-1');
+deck1Button.addEventListener('click', function() {
+    switchDecks(this.name);
+});
 const list = document.getElementById('list-container');
 const deleteButton = document.getElementById('delete-button');
 deleteButton.addEventListener('click', () => deleteList());
@@ -26,8 +30,8 @@ let noCards = {'question' : 'There are currently no cards in your deck. Add one 
 let index = 0;
 let deckIndex = 0;
 let animated = false;
-if (deck.length === 0) node = document.createTextNode(noCards.question);
-else node = document.createTextNode(deck[0].question);
+if (deckCollection[deckIndex].length === 0) node = document.createTextNode(noCards.question);
+else node = document.createTextNode(deckCollection[deckIndex][0].question);
 content = document.getElementById('question');
 content.appendChild(node);
 card.appendChild(content);
@@ -38,12 +42,12 @@ function flip() {
         content = card.children[0];
         if (content.id == 'question') {
             content.id = 'answer';
-            if (deck.length > 0) setTimeout(function() {content.innerText = deck[index].answer}, 600);
+            if (deckCollection[deckIndex].length > 0) setTimeout(function() {content.innerText = deckCollection[deckIndex][index].answer}, 600);
             else setTimeout(function() {content.innerText = noCards.answer}, 600);
         }
         else if (content.id == 'answer') {
             content.id = 'question';
-            if (deck.length > 0) setTimeout(function() {content.innerText = deck[index].question}, 600);
+            if (deckCollection[deckIndex].length > 0) setTimeout(function() {content.innerText = deckCollection[deckIndex][index].question}, 600);
             else setTimeout(function() {content.innerText = noCards.question}, 600);
         }
     }
@@ -58,24 +62,24 @@ function unflip() {
 
 function next() {
     content = card.children[0];
-    if (deck.length > 0) {
+    if (deckCollection[deckIndex].length > 0) {
         index++;
-        if (index >= deck.length) {
+        if (index >= deckCollection[deckIndex].length) {
             index = 0;
         }
         content.id = 'question';
-        content.innerText = deck[index].question;
+        content.innerText = deckCollection[deckIndex][index].question;
     }
     else content.innerText = noCards.question;
 }
 
 function back() {
     content = card.children[0];
-    if (deck.length > 0) {
+    if (deckCollection[deckIndex].length > 0) {
         index--;
-        if (index < 0) index = deck.length - 1;
+        if (index < 0) index = deckCollection[deckIndex].length - 1;
         content.id = 'question';
-        content.innerText = deck[index].question;
+        content.innerText = deckCollection[deckIndex][index].question;
     }
     else content.innerText = noCards.question;
 }
@@ -83,12 +87,12 @@ function back() {
 function addCard() {
     if (frontText.value === '' || backText.value === '') alert('Neither side of a card can be empty.');
     else {
-        deck.push({'question' : frontText.value, 'answer' : backText.value});
+        deckCollection[deckIndex].push({'question' : frontText.value, 'answer' : backText.value});
         frontText.value = '';
         backText.value = '';
         alert('Card added successfully.');
         updateList();
-        if (deck.length === 1) next();
+        if (deckCollection[deckIndex].length === 1) next();
     }
     localStorage.setItem('deck', JSON.stringify(deck));
 }
@@ -101,29 +105,29 @@ function deleteList() {
         if (checkboxes[i].checked == true) deleteList.push(questions[i].innerText);
     }
     if (deleteList.length === 0) return;
-    for (let i = 0; i < deck.length; i++){
-        if (Object.values(deck[i])[0] === deleteList[0]) {
-            deck.splice(i, 1);
+    for (let i = 0; i < deckCollection[deckIndex].length; i++){
+        if (Object.values(deckCollection[deckIndex][i])[0] === deleteList[0]) {
+            deckCollection[deckIndex].splice(i, 1);
             deleteList.shift();
             i = -1;
         }
     }
     updateList();
-    if (deck.length === 0) flip();
+    if (deckCollection[deckIndex].length === 0) flip();
     localStorage.setItem('deck', JSON.stringify(deck));
 }
 
 function shuffle() {
-    if (deck.length === 0) return;
+    if (deckCollection[deckIndex].length === 0) return;
     else {
         for (let i = deck.length - 1; i > 0; i--) {
             // Generate a random index between 0 and i
             const randomIndex = Math.floor(Math.random() * (i + 1));
             // Swap elements at randomIndex and i
-            [deck[i], deck[randomIndex]] = [deck[randomIndex], deck[i]];
+            [deckCollection[deckIndex][i], deckCollection[deckIndex][randomIndex]] = [deckCollection[deckIndex][randomIndex], deckCollection[deckIndex][i]];
         }
         content = card.children[0];
-        content.innerText = deck[index].question;
+        content.innerText = deckCollection[deckIndex][index].question;
         updateList();
     }
 }
@@ -133,12 +137,17 @@ function makeNewDeck() {
     newTab = document.createElement('button');
     deckCollection.push([]);
     newTab.innerText = "Deck " + deckCollection.length;
-    newTab.id = "deck-" +deckCollection.length;
-    newTab.addEventListener('click', () => switchDecks(this.id));
+    newTab.id = "deck-" + deckCollection.length;
+    newTab.name = deckCollection.length;
+    newTab.addEventListener('click', function() {
+        switchDecks(this.name);
+    });
     tabs.appendChild(newTab);
 }
 
 function switchDecks(deckId) {
+    deckIndex = deckId-1;
+    updateList();
 }
 
 function updateList() {
@@ -148,18 +157,17 @@ function updateList() {
         questions[0].remove();
         labels[0].remove();
     }
-    for (let i = 0; i < deck.length; i++) {
+    for (let i = 0; i < deckCollection[deckIndex].length; i++) {
         listQuestion = document.createElement('input');
         listQuestion.type = 'checkbox';
         listQuestion.id = `q${i}`;
         listQuestion.name = `q${i}`;
-        listQuestion.value = deck[i].question;
+        listQuestion.value = deckCollection[deckIndex][i].question;
         listQuestion.classList.add('question');
         listQuestionLabel = document.createElement('label');
         listQuestionLabel.setAttribute('for', listQuestion.id)
-        listQuestionLabel.innerText = deck[i].question;
+        listQuestionLabel.innerText = deckCollection[deckIndex][i].question;
         list.appendChild(listQuestion);
         list.appendChild(listQuestionLabel);
-
     }
 }
